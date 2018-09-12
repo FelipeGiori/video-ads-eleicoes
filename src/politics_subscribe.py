105cec3d2dd4f7efcb46ee2a0af93f2efa8a4dd8
Guilherme Soares <guilhermesoares@dcc.ufmg.br>
2018-09-12 16:36:48 -0300

    Script para inscrever-se em canais agora pega dados do banco

--- src/politics_subscribe.py
+++ src/politics_subscribe.py
@@ -2,43 +2,77 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
+from pyvirtualdisplay import Display
 import pandas as pd
 from time import sleep
+from requests import get
+from database_model import Persona
 
-# Conectar ao bando de dados e pegar o alinhamento da persona
-channels = pd.read_csv("personas_politics_channels/rightwing.csv")
+def get_public_ip():
+    return get('https://ipapi.co/ip/').text
 
-profile = webdriver.FirefoxProfile()
-driver = webdriver.Firefox(firefox_profile = profile)
+def setup_driver():
+    profile = webdriver.FirefoxProfile()
+    driver = webdriver.Firefox(firefox_profile = profile)
+    return driver
 
-# Get credentials from database
-name = ''
-password = ''
 
-def login_youtube(driver):
+def driver_quit(driver):
+    driver.quit()
+
+
+def login_youtube(driver, name, password):
         driver.get('https://accounts.google.com/')
         driver.find_element_by_id('identifierId').send_keys(name)
         driver.find_element_by_id('identifierNext').click()
         driver.window_handles[0]
-        sleep(10) # Espera a transição de formulário
+        sleep(5) # Espera a transição de formulário
         driver.find_element_by_name('password').send_keys(password)
         driver.find_element_by_id('passwordNext').click()
         sleep(5)
         logged_in_url = "https://myaccount.google.com/?pli=1"
         if(driver.current_url == logged_in_url):
             driver.get('https://youtube.com')
-            return True
         else:
             print("Could not log in")
-            return False
         
-login_youtube(driver)
 
-for _, row in channels.iterrows():
-    driver.get(row['channel'])
-    try:
-        driver.find_element_by_id("subscribe-button").click()
-    except:
-        print("Couldn't find subscribe button")
+def subscribe(driver, name, password, channels):
+    login_youtube(driver, name, password)
+    
+    for _, row in channels.iterrows():
+        driver.get(row['channel'])
+        try:
+            driver.find_element_by_id("subscribe-button").click()
+            print("Subscribed to {}".format(row['channel']))
+        except:
+            print("Couldn't find subscribe button of {}".format(row['channel']))
 
-driver.quit()
+
+def main():
+    display = Display(visible = False, size=(800, 600), backend='xvfb').start()
+    
+    ip = get_public_ip()
+    personas = Persona.select().where(Persona.source_ip == ip)
+    
+    for persona in personas:
+        if(persona.political_wing == 'right'):
+            channels = pd.read_csv("personas_politics_channels/rightwing.csv")
+        elif(persona.political_wing == 'left'):
+            channels = pd.read_csv("personas_politics_channels/leftwing.csv")
+        elif(persona.political_wing == 'center'):
+            channels = pd.read_csv("personas_politics_channels/center.csv")
+        else:
+            print("Error! Cound not find persona alignment or channel file")
+            
+        driver = setup_driver()
+
+        subscribe(driver, persona.email, persona.password, channels)
+        
+        driver_quit(driver)
+    
+    display.popen.terminate()
+
+    
+if __name__ == "__main__":
+    main()
